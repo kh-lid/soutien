dbd0f690f6fb27c10a32f897905486552004e985
kh-lid <81291966+kh-lid@users.noreply.github.com>
2022-09-17 16:26:29 +0100

    Update Apropos.php

--- Apropos.php
+++ Apropos.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-compatible" content="ie-edge">
 	<link rel="stylesheet" type="text/css" href="css/home1.css">
-		<title>Apropos de nous </title>
+		<title>QUI SUIS-JE  </title>
 </head>
 <body>
 	<header>
@@ -15,8 +15,8 @@
 		</div>
 		<ul class="main-nav">
 			<li ><a href="http://sout-ien.herokuapp.com/index.php">Accueil</a></li>
-			<li><a href="./service.php">Services</a></li>
-			<li class="act"><a href="http://sout-ien.herokuapp.com/Apropos.php">Apropos de nous</a></li>
+			<li><a href="./service.php">LES COURS</a></li>
+			<li class="act"><a href="http://sout-ien.herokuapp.com/Apropos.php">QUI SUIS-JE </a></li>
 			<li><a href="http://sout-ien.herokuapp.com/contacter.php">Contacter </a></li>
 		</ul>
 		
@@ -25,13 +25,13 @@
 	<main>
 		<article style="background-color: linen">
 <p>
-			<h2 style="text-align: center;">Apropos de nous <br></h2><br>
-			<h2 style="text-align: center;"> nous somme un groupe des ensigenants motivés des <b>Mathématiques</b> ,<b>Informatique </b>,<b>Anglais</b> et <b>physique</b> avec une exprience de <em>5 ans</em></h2> <br>
-			<h3><li>donnant des cours de soutien pour tous les niveaux (primaire ,collège,lycée ,supérieur,.........)</li> </h3> 
-			<h3><li> travaillant avec tablette graphique et un tableau blanc virtuel ce qui garantit la fluidité du cours.</li> </h3> 
-			<h3><li>apportant une grande importance au suivi de l'élève. Grâce une rapide relecture du cours avec celui-ci, je repère les points bloquants. Après une explication adaptée à chacun, on passe à des exercices pratiques pour ancrer les connaissances. La séance suivante, on vérifie que les connaissances sont toujours maîtrisées avant de passer à la suite.</li> </h3> 
-			<h3><li>réalisant des exemples des projets en programmation avec l'éléve</li></h3>
-
+			<h2 style="text-align: center;">QUI SUIS-JE  <br></h2><br>
+			<h2 style="text-align: center;"> Titulaire d’une licence en informatique à l’université de Ouarzazate, je m’appelle Khalid, grand passionné par les mathématiques, l’informatique, l’anglais et la physique que je pratique depuis maintenant de 5 ans. </h2> <br>
+			<h3><li>Je donne des cours de soutien pour tous les niveaux (primaire, collège, lycée, supérieur…)</li> </h3> 
+			<h3><li>Concernant le déroulement des cours, je propose des cours particuliers individuels ou des travaux de groupe, selon vos préférences.   </li> </h3> 
+			<h3><li>Je travaille avec une tablette graphique et un tableau blanc virtuel, garantissant ainsi la fluidité du cours. </li> </h3> 
+			<h3><li>J’accorde une grande importance au suivi de l'élève. Grâce une rapide relecture du cours avec celui-ci, je repère les points bloquants. Après une explication adaptée à chacun, je passe à des exercices pratiques pour ancrer les connaissances. La séance suivante, je vérifie que les connaissances sont toujours maîtrisées avant de passer à la suite.</li></h3>
+			<h3><li>Nous réalisons des exemples de projets en programmation </li> </h3>
 
 </p>
 	</article>

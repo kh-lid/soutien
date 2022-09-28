e3169b779a321a0fe128f61aa059d5ad13ac4d16
kh-lid <81291966+kh-lid@users.noreply.github.com>
2022-09-28 21:42:54 +0100

    Update service.php

--- service.php
+++ service.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-compatible" content="ie-edge">
 	<link rel="stylesheet" type="text/css" href="css/service.css">
 		<title>LES COURS</title>
+	<link rel="icon" type="image/x-icon" href="https://findicons.com/files/icons/2166/oxygen/128/application_vnd_oasis_opendocument_formula.png">
 </head>
 <body>
 	<header>

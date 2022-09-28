643617e3ab70948d9f10c2bd67de4cd4c0906886
kh-lid <81291966+kh-lid@users.noreply.github.com>
2022-09-28 21:38:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-compatible" content="ie-edge">
 	<link rel="stylesheet" type="text/css" href="css/home1.css">
 		<title>Cours de soutien</title>
+	<link rel="icon" type="image/x-icon" href="https://findicons.com/files/icons/2166/oxygen/128/application_vnd_oasis_opendocument_formula.png">
 </head>
 <body>
 	<header>
